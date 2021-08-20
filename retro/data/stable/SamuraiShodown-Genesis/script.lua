5d2efa49f4a62c9b6308c93da6ca30efe7b629f5
Şevket Umut Çakır <cakiaccounts@gmail.com>
2021-08-20 15:01:02 +0300

    ss script

--- retro/data/stable/SamuraiShodown-Genesis/script.lua
+++ retro/data/stable/SamuraiShodown-Genesis/script.lua
@@ -1,4 +1,27 @@
 previous_score = 0
+ph = 128
+eph = 128
+el = 0
+
+function calculate_reward ()
+    local r = 0
+    if data.health ~= 128 and data.health ~= 0 then
+        local delta = data.health - ph
+        r = r + delta
+    end
+    if data.enemy_health ~= 128 and data.enemy_health ~= 0 then
+        local delta = data.enemy_health - eph 
+        r = r - delta
+    end
+    if data.enemy_level ~=0 then
+        local delta = data.enemy_level - el
+        r = r + delta*100
+    end
+    el = data.enemy_level
+    eph = data.enemy_health
+    ph = data.health
+    return r
+end
 
 function correct_score ()
   if data.score > previous_score then
